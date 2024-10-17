868f2885bfec56ed77f5285b22a9c51e3b4abcb9
MGergo20 <medgyaszayg3@gmail.com>
2024-10-17 11:17:29 +0200

    Többi

--- feladatok.py
+++ feladatok.py
@@ -5,6 +5,8 @@ Az alábbi metódusok paraméterként kapják a fenti listát
 2. Mennyi a negatív számok összege?
 3. Mennyi az öttel osztható számok átlaga?
 """
+import random
+
 
 def poz_szamok_szama(lista=[]):
 
@@ -37,4 +39,67 @@ def öttel_osztható(lista):
             db+=1
         i+=1
     return osszeg
+    
+
+
+def ermedobas():
+    eredmeny_lista=[]
+    i:int=0
+    while(i<10):
+        erem:int=int(random.random()*2) +1
+        if(erem==1):
+            eredmeny_lista.append("Fej")
+        elif(erem==2):
+            eredmeny_lista.append("írás")
+        i+=1
+    return eredmeny_lista
+
+
+def fej_dobasok(eredmeny_lista):
+    i:int=0
+    fej:int=0
+
+    while(i<len(eredmeny_lista)):
+        if(eredmeny_lista[i]=="Fej"):
+            fej +=1
+        i+=1
+    return fej
+
+def kockadobas():
+    kocka_lista=[]
+    i:int=0
+    while(i<200):
+        kocka:int=int(random.random()*7) +1
+        if(kocka==1):
+            kocka_lista.append(1)
+        elif(kocka==2):
+            kocka_lista.append(2)
+        elif(kocka==3):
+            kocka_lista.append(3)
+        elif(kocka==4):
+            kocka_lista.append(4)
+        elif(kocka==5):
+            kocka_lista.append(5)
+        elif(kocka==6):
+            kocka_lista.append(6)
+        i+=1
+    return kocka_lista
+        
+def kockaszamok(kocka_lista):
+    i:int=0
+    egy:int=0
+    ketto:int=0
+    harom:int=0
+    negy:int=0
+    ot:int=0
+    hat:int=0
+    while (i < len(kocka_lista)):
+        if(kocka_lista[i]==1):
+            egy+=1
+        elif(kocka_lista[i]==2):
+            ketto+=1
+        
+
+
+
     
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import feladatok
+
 lista=[12, 21, 56, 32, -5, -23, 35]
 
 
@@ -14,8 +15,23 @@ print("2. Feladat")
 db:int=feladatok.negativ_szamok_osszege(lista)
 print(f"A negatív számok összege {db}")'''
 
-print("3. Feladat")
+'''print("3. Feladat")
 atlag:float=feladatok.öttel_osztható(lista)
 print(f"Az 5-tel osztható számok átlaga: {atlag}")
+
+
+print("4. Feladat")
+eredmeny_lista=feladatok.ermedobas()
+print(f"Érmék: {eredmeny_lista}")
+
+print("4. Feladat 2-es része")
+fej=feladatok.fej_dobasok(eredmeny_lista)
+print(f"Ennyi fej volt: {fej}")'''
+
+print("5. Feladat")
+kocka_lista=feladatok.kockadobas()
+print(f"Dobások: {kocka_lista}")
+
+
 
 
